8605c846e4d0d49af4ac1aab356df7a19739d3c0
ZudaPradana <31976928+ZudaPradana@users.noreply.github.com>
2017-09-22 10:00:44 +0700

    Add files via upload

---
+++ Tugas1_04217044.py
@@ -0,0 +1,10 @@
+nama = "Zuda Pradana Putra"
+nim = "04217044"
+umur = 18
+print("Namaku :" + nama)
+print("Nim :" + nim)
+print("Umur")
+print(umur)
+print("Umur 5 Tahun ke Depan")
+print(umur + 5)
+
